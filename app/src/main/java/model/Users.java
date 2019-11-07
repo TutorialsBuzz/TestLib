4264f49bf8f295431fc9108f6289751335da5aad
Pawan Deshpande <Pawan.Deshpande@ril.com>
2019-11-07 18:11:48 +0530

    first commit

---
+++ app/src/main/java/model/Users.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Users {
+
+    private String firstnName;
+    private String lastName;
+    private int age;
+
+    public String getFirstnName() {
+        return firstnName;
+    }
+
+    public void setFirstnName(String firstnName) {
+        this.firstnName = firstnName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
